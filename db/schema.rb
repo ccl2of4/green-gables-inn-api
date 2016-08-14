f7efb1effd0bb8ae702ff3ee4b0c2d906cd5a86d
Connor Lirot <ccl2of4@tx.rr.com>
2016-08-13 21:36:12 -0500

    use postgres instead of sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160731155248) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "suites", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
